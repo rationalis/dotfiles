050be9dcf9624dfaba5dee1223f3e798c32d5cf5
Jimmy Ye <jiy162@ucsd.edu>
2020-03-14 02:07:53 -0700

    Actually add betterquit (I forgot :))

---
+++ .spacemacs.d/betterquit.el
@@ -0,0 +1,28 @@
+(defun keyboard-quit-context+ ()
+  "Quit current context.
+
+    This function is a combination of `keyboard-quit' and
+    `keyboard-escape-quit' with some parts omitted and some custom
+    behavior added."
+  (interactive)
+  (cond ((region-active-p)
+         ;; Avoid adding the region to the window selection.
+         (setq saved-region-selection nil)
+         (let (select-active-regions)
+           (deactivate-mark)))
+        ((eq last-command 'mode-exited) nil)
+        (current-prefix-arg
+         nil)
+        (defining-kbd-macro
+          (message
+           (substitute-command-keys
+            "Quit is ignored during macro defintion, use \\[kmacro-end-macro] if you want to stop macro definition"))
+          (cancel-kbd-macro-events))
+        ((active-minibuffer-window)
+         (when (get-buffer-window "*Completions*")
+           ;; hide completions first so point stays in active window when
+           ;; outside the minibuffer
+           (minibuffer-hide-completions))
+         (abort-recursive-edit))
+        (t
+         (keyboard-quit))))
